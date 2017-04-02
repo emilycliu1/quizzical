99db3e32ac6bb62dfe1f27fd94e7a4269bbd2fbf
emilycliu1 <emilycliu1@gmail.com>
2017-04-02 02:29:36 -0400

    buttons scroll

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+$(document).ready(function(){
 
+	// SUBMIT BUTTON ON 1ST PAGE
+	$("#submit_1").click(function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 800}, 600);
+	});
+
+	// PREVIOUS BUTTON ON 2ND PAGE
+	$("#prev_2").click(function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 0}, 600);
+	});
+
+	// SUBMIT BUTTON ON 2ND PAGE
+	$("#submit_2").click(function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 1600}, 600);
+	});
+
+	// PREVIOUS BUTTON ON 3RD PAGE
+	$("#prev_3").click(function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 800}, 600);
+	});
+
+	// SUBMIT BUTTON ON 3RD PAGE
+	$("#submit_3").click(function(e){
+		e.preventDefault();
+		$("html, body").animate({scrollTop: 2400}, 600);
+	});
+
+
+}); // ends $(document).ready(funtion(){
